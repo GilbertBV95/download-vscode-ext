5a033788de7847d593a4f48be1477a2a9acb6056
GilbertBV95 <gilbertbv95@gmail.com>
2025-02-25 14:51:58 -0500

    agregados estilos a las template agregado bootstrap a las template agregado el javascritp para la descarga automatica del fichero en una nueva pestanha eliminando console.log

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import morgan from 'morgan';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
-import axios from 'axios';
 
 const app = express();
 const port = 3000;
@@ -13,17 +12,18 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 app.set('view engine', 'pug');
 app.set('views', join(__dirname, 'views'));
 app.enable('view_cache');
-
-app.use(express.static(join(__dirname, 'public')))
+app.locals.pretty = true;
+app.locals.tabs = true;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('tiny'));
 
-const datos = {};
+const datos = {
+	title: 'Descargar extension  para VSCode'
+};
 
 app.get('/', (req, res) => {
-	datos.title = 'Descargar extension  para VSCode';
 	res.render('index', { datos })
 })
 
@@ -39,16 +39,22 @@ app.post('/getExtension', async (req, res) => {
 			const dividePubExt = pubExt.split('.')
 			const publisher = dividePubExt[0];
 			const extension = dividePubExt[1];
-			const vsExtUrl = `https://marketplace.visualstudio.com/_apis/public/gallery/publishers/${publisher}/vsextensions/${extension}/${version}/vspackage`;
-			const respuesta = await axios.get(vsExtUrl);
-			console.log(respuesta)
+			if (publisher && extension) {
+				datos.url = `https://marketplace.visualstudio.com/_apis/public/gallery/publishers/${publisher}/vsextensions/${extension}/${version}/vspackage`;
+				datos.timeToclick = true;
+			} else {
+				datos.timeToclick = false;
+				datos.error = false;
+				datos.url = '';
+			}
 		} else {
-			res.send('Url incorrecta');
+			datos.timeToclick = false;
+			datos.url = '';
+			datos.error = 'Url incorrecta';
 		}
 
-		res.render('index', { message: 'Hola todo perfecto' });
-	} catch (error) {
-		console.log(err)
+		res.render('index', { datos });
+	} catch (err) {
 		res.status(500).send(err.msg || err.code || 'Error al obtener la extension');
 	}
 })
